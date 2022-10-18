580f2ba7dc1f2026df0cae978c8d2f1426cfeecd
isabella-arteaga-yssy <112958364+isabella-arteaga-yssy@users.noreply.github.com>
2022-10-18 14:49:28 -0300

    remove

--- olar/src/Componente00.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Componente00 extends React.Component{
-    render(){
-        return <h1>Meu primeiro primeiro talvez</h1>
-    }
-}
-
-export default Componente00;
--- olar/src/Componente01.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Componente01 extends React.Component{
-    render(){
-        return <h1>Meu primeiro segundo hoje.</h1>
-    }
-}
-
-export default Componente01;
